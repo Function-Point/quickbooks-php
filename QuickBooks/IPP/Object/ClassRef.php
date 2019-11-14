47ed79981f7f604b682f907269d9b091ff3f7ed8
Paul Lin <paul.lin@functionpoint.com>
2019-11-14 11:19:58 -0800

    Task FPP-736 QBO OAuth2

---
+++ QuickBooks/IPP/Object/ClassRef.php
@@ -0,0 +1,7 @@
+<?php
+QuickBooks_Loader::load('/QuickBooks/IPP/Object.php');
+
+class QuickBooks_IPP_Object_ClassRef extends QuickBooks_IPP_Object
+{
+
+}
---
+++ QuickBooks/IPP/Object/ParentRef.php
@@ -0,0 +1,7 @@
+<?php
+QuickBooks_Loader::load('/QuickBooks/IPP/Object.php');
+
+class QuickBooks_IPP_Object_ParentRef extends QuickBooks_IPP_Object
+{
+
+}
